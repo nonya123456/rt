05a8d4b247df1c89f95514222e98dd96be8a41cb
nonya123456 <nonya579@gmail.com>
2026-02-15 13:07:52 +0700

    add camera and bg

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,25 @@ const std = @import("std");
 const Ray = @import("Ray.zig");
 
 pub fn main(init: std.process.Init) !void {
-    _ = Ray{ .origin = .{ 0, 0, 0 }, .dir = .{ 0, 0, 0 } };
+    const aspect_ratio = 16.0 / 9.0;
+    const image_width: i32 = 400;
+    const image_height: i32 = @intFromFloat(@as(f64, @floatFromInt(image_width)) / aspect_ratio);
 
-    const image_width = 256;
-    const image_height = 256;
+    const focal_length = 1.0;
+    const viewport_height = 2.0;
+    const viewport_width = viewport_height *
+        @as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height));
+    const camera_center: Vec3 = .{ 0, 0, 0 };
+
+    const viewport_u: Vec3 = .{ viewport_width, 0, 0 };
+    const viewport_v: Vec3 = .{ 0, -viewport_height, 0 };
+
+    const pixel_delta_u = viewport_u / splat3(@floatFromInt(image_width));
+    const pixel_delta_v = viewport_v / splat3(@floatFromInt(image_height));
+
+    const viewport_upper_left = camera_center - Vec3{ 0, 0, focal_length } -
+        viewport_u / splat3(2.0) - viewport_v / splat3(2.0);
+    const pixel00_loc = viewport_upper_left + splat3(0.5) * (pixel_delta_u + pixel_delta_v);
 
     var buf: [4096]u8 = undefined;
     var file = try std.Io.Dir.cwd().createFile(init.io, "./img.ppm", .{});
@@ -27,9 +42,12 @@ pub fn main(init: std.process.Init) !void {
         std.debug.print("Scanlines remaining: {}\n", .{image_height - j});
         var i: usize = 0;
         while (i < image_width) : (i += 1) {
-            const r = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(image_width - 1));
-            const g = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(image_height - 1));
-            try writeColor(init.gpa, writer, .{ r, g, 0.0 });
+            const pixel_center = pixel00_loc + (splat3(@floatFromInt(i)) * pixel_delta_u) +
+                (splat3(@floatFromInt(j)) * pixel_delta_v);
+            const ray_direction = pixel_center - camera_center;
+            const r: Ray = .{ .origin = camera_center, .dir = ray_direction };
+            const color = rayColor(r);
+            try writeColor(init.gpa, writer, color);
         }
     }
 
@@ -37,7 +55,28 @@ pub fn main(init: std.process.Init) !void {
     std.debug.print("Done\n", .{});
 }
 
-fn writeColor(gpa: std.mem.Allocator, writer: *std.Io.Writer, color: @Vector(3, f64)) !void {
+const Vec3 = @Vector(3, f64);
+
+fn rayColor(r: Ray) Vec3 {
+    const unit_direction = unitVector(r.dir);
+    const a = 0.5 * (unit_direction[1] + 1.0);
+    return splat3(1.0 - a) + splat3(a) * Vec3{ 0.5, 0.7, 1.0 };
+}
+
+fn splat3(scalar: f64) Vec3 {
+    return @as(Vec3, @splat(scalar));
+}
+
+fn unitVector(v: Vec3) Vec3 {
+    var v_sq = v * v;
+    var s: f64 = 0;
+    s += v_sq[0];
+    s += v_sq[1];
+    s += v_sq[2];
+    return v / splat3(@sqrt(s));
+}
+
+fn writeColor(gpa: std.mem.Allocator, writer: *std.Io.Writer, color: Vec3) !void {
     const ir: i32 = @intFromFloat(255.999 * color[0]);
     const ig: i32 = @intFromFloat(255.999 * color[1]);
     const ib: i32 = @intFromFloat(255.999 * color[2]);
